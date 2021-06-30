ff25c4f18a6ae49702252722daea793b6fac9fc6
Gustavo Mendes <guguren3@hotmail.com>
2021-06-30 18:10:02 -0300

    first commit

---
+++ GerenciadorCondominios.DAL/ConfiguracaoRepositoriosExtension.cs
@@ -0,0 +1,19 @@
+﻿using GerenciadorCondominios.DAL.Interfaces;
+using GerenciadorCondominios.DAL.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominios.DAL
+{
+    public static class ConfiguracaoRepositoriosExtension
+    {
+
+        public static void ConfigurarRepositorios(this IServiceCollection services)
+        {
+            services.AddTransient<IUsuarioRepositorio, UsuarioRepositorio>();
+            services.AddTransient<IFuncaoRepositorio, FuncaoRepositorio>();
+        }
+    }
+}
